d462ef4939c95a6e9aef7242ae39e8afc0aeedc8
p-laranjinha <plcasimiro2000@gmail.com>
2024-06-14 01:20:46 +0100

    The shortcut now works inside iframes.

--- FMovies Skip Shortcut/README.md
+++ FMovies Skip Shortcut/README.md
@@ -1,3 +1,3 @@
 Adds the "-" key as a shortcut to skip to the next episode on the FMovies website.
 
-Shortcut only works when both the video player and the search bar are not focused. The first one because it being an iframe doesn't allow me to add the required event listener without additional user work and security risks.
+Shortcut will only trigger when an input is not focused.
--- FMovies Skip Shortcut/main.user.js
+++ FMovies Skip Shortcut/main.user.js
@@ -2,22 +2,87 @@
 // @name        FMovies Skip Shortcut
 // @license     MIT
 // @namespace   rtonne
-// @match       https://fmoviesz.to/*
-// @grant       none
-// @version     1.0
+// @match       https://*/*
+// @icon        https://www.google.com/s2/favicons?sz=64&domain=fmovies24.to
+// @version     1.1
 // @author      Rtonne
 // @description Adds a shortcut ("-") to skip to the next episode
 // @run-at      document-end
+// @grant       GM.setValue
+// @grant       GM.getValue
+// @grant       GM.addValueChangeListener
 // ==/UserScript==
 
-document.addEventListener("keydown", (event) => {
-  // If key pressed is "-" and the search box is not focused, continue
-  if (
-    event.code !== "Minus" ||
-    document.querySelector("div.search-box input") === document.activeElement
-  ) {
+// Movies are probably not be needed, but there might an exception so I'll include them
+const URL_REGEX = /^https:\/\/fmovies24.to\/((tv)|(movie))\/.+$/;
+const IS_FMOVIES_VALUE_KEY = "is_FMovies";
+const NEXT_EPISODE_VALUE_KEY = "next_episode";
+
+if (window.top === window.self) {
+  // Run this with regular websites
+  topWebsite();
+} else {
+  // Run this with iframes
+  iFrame();
+}
+
+function topWebsite() {
+  const isFMovies = !URL_REGEX.test(window.location.href);
+  // If top website is not FMovies, make it known and return
+  if (isFMovies) {
+    GM.setValue(IS_FMOVIES_VALUE_KEY, false);
     return;
   }
+
+  // Add a focus event listener to the window because changing tabs can set "is_FMovies" to false
+  window.addEventListener("focus", () => {
+    GM.setValue(IS_FMOVIES_VALUE_KEY, true);
+  });
+  // Add a blur event listener to the window so that another tab's iframe
+  // shouldn't get the keydown event listener added
+  window.addEventListener("blur", () => {
+    GM.setValue(IS_FMOVIES_VALUE_KEY, false);
+  });
+
+  GM.setValue(IS_FMOVIES_VALUE_KEY, true);
+  addKeydown();
+
+  GM.setValue(NEXT_EPISODE_VALUE_KEY, false);
+  GM.addValueChangeListener(NEXT_EPISODE_VALUE_KEY, (_, __, next_episode) => {
+    if (next_episode) {
+      GM.setValue(NEXT_EPISODE_VALUE_KEY, false);
+      goToNextEpisode();
+    }
+  });
+}
+
+function iFrame() {
+  let isFMovies = GM.getValue(IS_FMOVIES_VALUE_KEY, false);
+  // This commented code section shouldn't be required because iframes are loaded by the user.
+  // And if it wasn't commented, there was the risk of another window's iframes having this event listener added.
+  // if (!isFMovies) {
+  //   GM.addValueChangeListener(IS_FMOVIES_VALUE_KEY, (_, __, isFMovies) => {
+  //     if (isFMovies) {
+  //       addKeydown();
+  //     }
+  //   });
+  // }
+  if (isFMovies) {
+    addKeydown();
+  }
+}
+
+function addKeydown() {
+  document.addEventListener("keydown", (event) => {
+    // If the correct key is pressed and an input is not focused, continue
+    if (event.key !== "-" || event.target.nodeName.toLowerCase() === "input") {
+      return;
+    }
+    GM.setValue(NEXT_EPISODE_VALUE_KEY, true);
+  });
+}
+
+function goToNextEpisode() {
   // If we can't find the episode in the list, abort and warn user
   if (document.querySelector("section#episodes li > a.active") === null) {
     alert("Current episode not found in list.");
@@ -29,7 +94,7 @@ document.addEventListener("keydown", (event) => {
   for (const element of episodeList) {
     if (currentEpisodeFound) {
       window.location.href =
-        "https://fmoviesz.to" + element.getAttribute("href");
+        window.location.origin + element.getAttribute("href");
       return;
     }
     if (element.className === "active") {
@@ -40,4 +105,4 @@ document.addEventListener("keydown", (event) => {
   if (currentEpisodeFound) {
     alert("Last episode reached.");
   }
-});
+}
