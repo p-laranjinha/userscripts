f02e885c742bd66fa0e80d208d3b87fea39328a0
p-laranjinha <plcasimiro2000@gmail.com>
2024-06-17 01:10:03 +0100

    Moved code to a library for use in other scripts

--- Toggle Youtube Styles/main.user.js
+++ Toggle Youtube Styles/main.user.js
@@ -4,9 +4,10 @@
 // @namespace   rtonne
 // @match       https://www.youtube.com/*
 // @icon        https://www.google.com/s2/favicons?sz=64&domain=youtube.com
-// @version     1.3
+// @version     1.5
 // @author      Rtonne
 // @description Adds toggles to enable/disable some styles that change Youtube
+// @require     https://update.greasyfork.org/scripts/498119/1395413/setupToggleCommands.js
 // @grant       GM.addStyle
 // @grant       GM.registerMenuCommand
 // @grant       GM.unregisterMenuCommand
@@ -14,24 +15,19 @@
 // @grant       GM.setValue
 // ==/UserScript==
 
-// To check if inplace command replacement is supported
-// https://violentmonkey.github.io/api/gm/#gm_registermenucommand
-const inplace =
-  "test" === GM.registerMenuCommand("test", () => {}, { id: "test" });
-GM.unregisterMenuCommand("test");
-
 const commands = [
   {
     id: "guide_sections",
-    disabled_caption: "☑ Guide Sections",
-    enabled_caption: "☐ Guide Sections",
-    disableStyle: () =>
+    default: true,
+    on_text: "☑ Guide Sections",
+    off_text: "☐ Guide Sections",
+    toggleOnFunction: () =>
       GM.addStyle(`
       #sections.ytd-guide-renderer > ytd-guide-section-renderer:nth-child(n+3) {
         display: unset;
       }
     `),
-    enableStyle: () =>
+    toggleOffFunction: () =>
       GM.addStyle(`
       #sections.ytd-guide-renderer > ytd-guide-section-renderer:nth-child(n+3) {
         display: none;
@@ -40,15 +36,16 @@ const commands = [
   },
   {
     id: "comments",
-    disabled_caption: "☑ Comments",
-    enabled_caption: "☐ Comments",
-    disableStyle: () =>
+    default: true,
+    on_text: "☑ Comments",
+    off_text: "☐ Comments",
+    toggleOnFunction: () =>
       GM.addStyle(`
       ytd-comments#comments {
         display: unset;
       }
     `),
-    enableStyle: () =>
+    toggleOffFunction: () =>
       GM.addStyle(`
       ytd-comments#comments {
         display: none;
@@ -57,15 +54,16 @@ const commands = [
   },
   {
     id: "related_videos",
-    disabled_caption: "☑ Related Videos",
-    enabled_caption: "☐ Related Videos",
-    disableStyle: () =>
+    default: true,
+    on_text: "☑ Related Videos",
+    off_text: "☐ Related Videos",
+    toggleOnFunction: () =>
       GM.addStyle(`
       #secondary.ytd-watch-flexy > #secondary-inner > #related {
         display: unset;
       }
     `),
-    enableStyle: () =>
+    toggleOffFunction: () =>
       GM.addStyle(`
       #secondary.ytd-watch-flexy > #secondary-inner > #related {
         display: none;
@@ -74,15 +72,16 @@ const commands = [
   },
   {
     id: "shorts",
-    disabled_caption: "☑ Shorts",
-    enabled_caption: "☐ Shorts",
-    disableStyle: () =>
+    default: true,
+    on_text: "☑ Shorts",
+    off_text: "☐ Shorts",
+    toggleOnFunction: () =>
       GM.addStyle(`
       ytd-rich-section-renderer:has(> div > [is-shorts]) {
         display: unset;
       }
     `),
-    enableStyle: () =>
+    toggleOffFunction: () =>
       GM.addStyle(`
       ytd-rich-section-renderer:has(> div > [is-shorts]) {
         display: none;
@@ -91,67 +90,22 @@ const commands = [
   },
   {
     id: "community",
-    disabled_caption: "☑ Community Posts",
-    enabled_caption: "☐ Community Posts",
-    disableStyle: () =>
+    default: true,
+    on_text: "☑ Community Posts",
+    off_text: "☐ Community Posts",
+    toggleOnFunction: () =>
       GM.addStyle(`
-      ytd-rich-section-renderer:has(> div > :not([is-shorts])) {
+      ytd-rich-section-renderer:has(> div > :not([is-shorts]):not([thumbnail-style])) {
         display: unset;
       }
     `),
-    enableStyle: () =>
+    toggleOffFunction: () =>
       GM.addStyle(`
-      ytd-rich-section-renderer:has(> div > :not([is-shorts])) {
+      ytd-rich-section-renderer:has(> div > :not([is-shorts]):not([thumbnail-style])) {
         display: none;
       }
     `),
   },
 ];
 
-for (const command of commands) {
-  setCommand(command);
-}
-registerAllCommands();
-
-async function registerAllCommands() {
-  for (const command of commands) {
-    GM.registerMenuCommand(
-      await getCaption(command),
-      () => toggleCommand(command),
-      { id: command.id, autoClose: false }
-    );
-  }
-}
-async function unregisterAllCommands() {
-  for (const command of commands) {
-    GM.unregisterMenuCommand(command.id);
-  }
-}
-async function toggleCommand(command) {
-  await GM.setValue(command.id, !(await GM.getValue(command.id, false)));
-  setCommand(command);
-  if (inplace) {
-    GM.registerMenuCommand(
-      await getCaption(command),
-      () => toggleCommand(command),
-      { id: command.id, autoClose: false }
-    );
-  } else {
-    unregisterAllCommands();
-    registerAllCommands();
-  }
-}
-async function setCommand(command) {
-  if (await GM.getValue(command.id, false)) {
-    command.enableStyle();
-  } else {
-    command.disableStyle();
-  }
-}
-async function getCaption(command) {
-  let caption = command.enabled_caption;
-  if (!(await GM.getValue(command.id, false))) {
-    caption = command.disabled_caption;
-  }
-  return caption;
-}
+setupToggleCommands(commands);
